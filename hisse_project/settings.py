a95c75be283275500012946c33128dd8460f124e
cem sinan ozturk <cemsinan@cems-MacBook-Pro.local>
2021-03-06 12:41:02 -0500

    initialize pages

--- hisse_project/settings.py
+++ hisse_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # Local
     'users.apps.UsersConfig', # new
+    'pages.apps.PagesConfig', # new
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'hisse_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # new
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
